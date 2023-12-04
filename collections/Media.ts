c4e5cc895da693e6e16daba084a338b75746c73b
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-12-05 00:16:13 +0300

    CMS scaffolding

--- collections/Media.ts
+++ collections/Media.ts
@@ -1,39 +1,39 @@
-import { CollectionConfig } from 'payload/types';
+import { CollectionConfig } from "payload/types";
 
 export type MediaType = {
-  filename: string
-  width: number
-  height: number
-  alt: string
+  filename: string;
+  width: number;
+  height: number;
+  alt: string;
   sizes: {
     card?: {
-      filename: string
-      width: number
-      height: number
-    }
+      filename: string;
+      width: number;
+      height: number;
+    };
     feature?: {
-      filename: string
-      width: number
-      height: number
-    }
-  }
-}
+      filename: string;
+      width: number;
+      height: number;
+    };
+  };
+};
 
 const Media: CollectionConfig = {
-  slug: 'media',
+  slug: "media",
   access: {
     read: (): boolean => true, // Everyone can read Media
   },
   upload: {
-    adminThumbnail: 'card',
+    adminThumbnail: "card",
     imageSizes: [
       {
-        name: 'card',
+        name: "card",
         width: 640,
         height: 480,
       },
       {
-        name: 'feature',
+        name: "feature",
         width: 1024,
         height: 576,
       },
@@ -41,9 +41,9 @@ const Media: CollectionConfig = {
   },
   fields: [
     {
-      name: 'alt',
-      label: 'Alt Text',
-      type: 'text',
+      name: "alt",
+      label: "Alt Text",
+      type: "text",
       required: true,
     },
   ],
--- payload.config.ts
+++ payload.config.ts
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 import Page from "./collections/Page";
 import Media from "./collections/Media";
 import FormSubmission from "./collections/FormSubmission";
+import Study from "./collections/Studies";
+import Category from "./collections/Category";
 
 dotenv.config();
 
 export default buildConfig({
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
-  collections: [Page, Media, FormSubmission],
+  collections: [Page, Study, Media, FormSubmission, Category],
 });

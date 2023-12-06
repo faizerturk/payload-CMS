dde134a9f997405c505dba18f31a2f4bfde28ccd
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-12-06 15:56:37 +0300

    footer & socialMedia

--- payload.config.ts
+++ payload.config.ts
@@ -6,11 +6,13 @@ import FormSubmission from "./collections/FormSubmission";
 import Study from "./collections/Studies";
 import Category from "./collections/Category";
 import MegaMenu from "./globals/MegaMenu";
+import Footer from "./globals/Footer";
+import SocialMedia from "./globals/SocialMedia";
 
 dotenv.config();
 
 export default buildConfig({
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
   collections: [Page, Study, Media, FormSubmission, Category],
-  globals: [MegaMenu],
+  globals: [MegaMenu, Footer, SocialMedia],
 });
